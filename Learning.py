9565f2c80e54cc57fb9f3350546d23b18b02b928
se27hdl-cmd <se27hdl@gmail.com>
2025-12-27 19:09:24 +0000

    Input, Conditonals and math examples

---
+++ Learning.py
@@ -0,0 +1,8 @@
+name = input("What is your name?")
+print("Hello", name)
+age = input("What is your age?")
+print("That is the same as me, name")
+
+age = input("How old are you?")
+if age == "23" :
+    print("You are 23")
---
+++ Learning2.py
@@ -0,0 +1,4 @@
+x = 10
+y = 5
+total = x + y
+print("The total list is", total)
