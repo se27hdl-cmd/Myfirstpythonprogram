405ac16cdd89921536a7743a7b20cefdb0c47153
se27hdl-cmd <se27hdl@gmail.com>
2025-12-29 12:04:41 +0000

    Added simple loop and while loop

---
+++ Forloop.py
@@ -0,0 +1,7 @@
+number = int(input("Choose a number between 1 and 10"))
+if number == 5 :
+  print("You chose the number 5")
+elif number < 1 or number > 10 :
+  print("Out of range")
+else : 
+  print("Valid number")
--- Loops.py
+++ Loops.py
@@ -2,6 +2,14 @@ for i in range (10) :
   print("Hello")
 
 seats = 10
-while seats < 0 :
+while seats > 0 :
   print("Sell Ticket")
   seats = seats - 1
+
+  seats = 300
+  while seats > 0 :
+    print("Sell Tickets")
+    seats = seats - 1
+
+for i in range (10)
+  print("Hello")
