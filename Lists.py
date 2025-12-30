623fdaef8076769d809daf15c06d831310891fb7
se27hdl-cmd <se27hdl@gmail.com>
2025-12-30 21:18:18 +0000

    Added lists

---
+++ Lists.py
@@ -0,0 +1,7 @@
+players = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank"]
+g1 = players[0:2]
+g2 = players[2:4]
+g3 = players[4:6]
+print("Group 1 :" + str(g1))
+print("Group 2 :" + str(g2))
+print("Group 3 :" + str(g3))
